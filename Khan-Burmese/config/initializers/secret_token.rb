fe3cd13ddcd6f71858d095b6a7e51f93881fcf82
jshum@berkeley.edu <jonathanashum@gmail.com>
2015-02-26 20:56:46 -0400

    Started Ruby App

---
+++ Khan-Burmese/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KhanBurmese::Application.config.secret_token = 'debcb5720930be3c94b20d28ae1a16d388ec2f8dfeee4f8245ea94a10dd47d0002b9b119bc826b372d6265b6967fa941c244b8830c9deb5b85cb17a2a7304575'
