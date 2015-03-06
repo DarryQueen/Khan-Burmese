9a5a9349cf6ff81f78c48ca1de9d98105c032611
DarryQueen <slimfrog@gmail.com>
2015-03-05 16:29:38 -0800

    Redirects users to previous page after login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_user!
+  before_filter :store_location, :authenticate_user!
+
+  # Set post-login path to previous URL:
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  private
+
+  # Store last URL for post-login redirect:
+  def store_location
+    return unless request.get?
+
+    ignore_paths = [
+      user_omniauth_authorize_path(:provider => 'facebook'),
+      user_omniauth_authorize_path(:provider => 'google_oauth2'),
+      new_user_session_path, new_user_registration_path,
+      new_user_password_path, edit_user_password_path,
+      destroy_user_session_path
+    ]
+
+    return if request.xhr? or request.path.match(Regexp.union(ignore_paths))
+    session[:previous_url] = request.fullpath
+  end
 end
