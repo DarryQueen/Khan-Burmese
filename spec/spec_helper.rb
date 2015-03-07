fb376db37310f290cafb591751a193a1563d2834
Edward Whang <ediwhang@gmail.com>
2015-03-07 02:07:55 +0000

    rake cucumber/spec tests now pass with skip_rails_admin_initializer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 SimpleCov.start 'rails'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV['SKIP_RAILS_ADMIN_INITIALIZER'] = 'false'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
