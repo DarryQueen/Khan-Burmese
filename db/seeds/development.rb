cadf5b0927e671a7dbab0de0a8e8fddd33770448
DarryQueen <slimfrog@gmail.com>
2015-03-06 21:06:23 -0800

    Updated seed files.

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,3 @@
 # Seed single administrator.
-user = User.create :email => 'admin@test.com', :password => 'administrator', :password_confirmation => 'administrator'
-user.toggle!(:admin)
+user = User.create :email => 'admin@test.com', :password => 'administrator', :password_confirmation => 'administrator', :role => 'superadmin'
 puts 'New user created: ' << user.email
