abce6292b8531caad99465d641f026f8259b2713
DarryQueen <slimfrog@gmail.com>
2015-03-13 23:24:38 -0700

    Videos searchable by tag and title.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,6 @@
 class Video < ActiveRecord::Base
   attr_accessible :description, :title, :youtube_id
+  acts_as_taggable
 
   def youtube_link
     "https://www.youtube.com/watch?v=#{self.youtube_id}"
@@ -7,7 +8,10 @@ class Video < ActiveRecord::Base
 
   def self.search(search)
     if search
-      Video.where('title LIKE ?', "%#{search}%")
+      tags = search.split
+      tagged_videos = Video.tagged_with(tags, :any => true)
+      title_videos = Video.where('title LIKE ?', "%#{search}%")
+      (tagged_videos + title_videos).uniq
     else
       scoped
     end
