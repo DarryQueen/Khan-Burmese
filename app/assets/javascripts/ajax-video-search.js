1487a31803899e0633c31f8fa48b36773fe54bbc
DarryQueen <slimfrog@gmail.com>
2015-03-14 02:44:57 -0700

    Nontrivial Javascript fix.

--- app/assets/javascripts/ajax-video-search.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('#video-search input').keyup(function() {
-    $.get($('#video-search').attr('action'), $('#video-search').serialize(), null, 'script');
-    return false;
-  });
-});
