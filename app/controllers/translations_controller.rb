0ab9c5648720b3249d7ab3baeb4bc70f712febe1
DarryQueen <slimfrog@gmail.com>
2015-04-03 18:31:03 -0700

    Show view includes dynamic Youtube, actions, and translate panels.

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,4 +1,17 @@
 class TranslationsController < ApplicationController
+  def create
+    @video = Video.find(params[:video_id])
+    @translation = Translation.new(:video => @video, :user => current_user)
+
+    unless @translation.save
+      @translation.errors.full_messages.each do |error|
+        add_flash(:alert, error)
+      end
+    end
+
+    redirect_to video_path @video
+  end
+
   def upload
     @video = Video.find(params[:video_id])
     @translation = Translation.find(params[:translation_id])
