8a27c6916bfe3b38e39a90e29f2fccdbff31bdc9
DarryQueen <slimfrog@gmail.com>
2015-04-06 18:11:53 -0700

    Updated registration fields to include names.

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,13 +34,8 @@ class User < ActiveRecord::Base
   def self.find_for_oauth(auth, signed_in_resource = nil)
     identity = Identity.find_for_oauth(auth)
 
-    # If a signed_in_resource is provided it always overrides the existing user
-    # to prevent the identity being locked with accidentally created accounts.
-    # Note that this may leave zombie accounts (with no associated identity)
-    # which can be cleaned up at a later date.
     user = signed_in_resource ? signed_in_resource : identity.user
 
-    # Create the user if needed:
     if user.nil?
       # Get the existing user by email:
       email = auth.info.email
@@ -48,9 +43,12 @@ class User < ActiveRecord::Base
 
       # Create the user if it's a new registration:
       if user.nil?
+        full_names = auth.info.name.rpartition(' ')
         user = User.new(
           :email => email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
-          :password => Devise.friendly_token[0, 20]
+          :password => Devise.friendly_token[0, 20],
+          :first_name => full_names.first,
+          :last_name => full_names.last
         )
         user.skip_confirmation! if user.respond_to?(:skip_confirmation)
         user.save!
