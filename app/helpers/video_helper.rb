12db526b735dab3d83bf7417e51e7f879802ef32
DarryQueen <slimfrog@gmail.com>
2015-04-10 20:20:48 -0700

    Implemented voting front end.

--- app/helpers/video_helper.rb
+++ app/helpers/video_helper.rb
@@ -25,4 +25,28 @@ module VideoHelper
 
     html.html_safe
   end
+
+  def toggle_vote_button(vote, video, translation, options = {})
+    voted = current_user.voted_as_when_voted_on(translation) == vote
+
+    vote_class = vote ? 'upvote' : 'downvote'
+
+    icon_class = vote ? 'fa-sort-asc' : 'fa-sort-desc'
+    icon_class += ' active' if voted
+
+    content_tag :span, :class => 'vote' do
+      link_to('', video_translation_vote_path(video, translation, :vote => vote), {
+        :'data-method' => :post,
+        :remote => true,
+        :class => "#{vote_class}-link"
+      }.merge(options)) + "<i class=\"fa fa-lg #{icon_class} #{vote_class}\"></i>".html_safe
+    end
+  end
+
+  def score_badge(score)
+    badge_class = 'badge vote-badge'
+    badge_class += ' danger' if score < 0
+
+    content_tag :span, score, :class => badge_class
+  end
 end
