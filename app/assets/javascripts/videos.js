705c1acbce2e537566a34d44ae9cf094016d064c
Brendan Smith <bwasmith@berkeley.edu>
2015-04-15 23:16:01 -0700

    Front end for video searching.

--- app/assets/javascripts/videos.js
+++ app/assets/javascripts/videos.js
@@ -1,8 +1,7 @@
 $(function() {
-  $('#video-search input').keyup(function() {
-    $.get($('#video-search').attr('action'), $('#video-search').serialize(), null, 'script');
-    return false;
-  });
+  $('#video-search input').keyup(videoSearch);
+  $('#status-filter input').change(videoSearch);
+
   $('#video-search input').keyup();
   $('#search').focus();
 
@@ -22,4 +21,9 @@ $(function() {
 function starClick(element) {
   var wrapper = $(element.parentNode);
   wrapper.html('<i class="fa fa-spinner fa-pulse fa-lg fa-fw"></i>');
+}
+
+function videoSearch() {
+  $.get($('#video-search').attr('action'), $('#video-search').serialize(), null, 'script');
+  return false;
 }
